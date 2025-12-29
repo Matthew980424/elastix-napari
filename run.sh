16f99861026c508ef02668eff802069e058131e1
Xiao Ma <maxiao769728450@gmail.com>
2025-12-29 14:24:05 +0100

    able to run

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-python scripts/run_par0029_step2_step3.py \
+python scripts/run_par0029.py \
   --fixed /home/x_ma/test_data/generated_pairs/N21VMR_abd_001/extracted_vols/vol_t000.nii.gz \
   --moving /home/x_ma/test_data/generated_pairs/N21VMR_abd_001/extracted_vols/vol_t001.nii.gz \
   --out /home/x_ma/test_data/groupwise_processed
